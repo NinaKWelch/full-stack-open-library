f742566ddb7b53a62f21d3e59e8d05718d730a52
NinaWelch <nwelch1@live.co.uk>
2023-01-06 04:15:52 +0000

    Exercise 8.5.

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -105,8 +105,17 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: (root, args) =>
-      args.author ? books.filter((book) => book.author === args.author) : books,
+    allBooks: (root, args) => {
+      if (args.author && args.genre) {
+        return books.filter((book) => book.author === args.author).filter((book) => book.genres.includes(args.genre))
+      } else if (args.author) {
+        return books.filter((book) => book.author === args.author)
+      } else if (args.genre) {
+        return books.filter((book) => book.genres.includes(args.genre))
+      } else {
+        return books
+      }
+    },
     allAuthors: () => authors.map((author) => {
       const getBookCount = () => {
         const booksByAuthor = books.filter((book) => book.author === author.name)
